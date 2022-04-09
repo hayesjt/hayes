07b379787b3e03a711e01f1faeed756958668413
Haylee Thomas-Kuhlmann <haylee430@gmail.com>
2022-04-09 16:10:05 -0500

    Adding Projects to dev work

--- src/components/TechVideoGallery.vue
+++ src/components/TechVideoGallery.vue
@@ -6,15 +6,158 @@
 
         <b-img src="../assets/headers/dev_project_header.png" fluid-grow></b-img>
 
+        <div class="border-needed">
+            <b-row>
 
-        <b-row class="title-space">
-            <b-col cols="6">
-                <project-tiles title="Project 1" header="year" footer="Tech used" cardtext="about" />
-            </b-col>
-            <b-col cols="6">
-                <project-tiles title="Project 2" header="year" footer="Tech used" cardtext="about" />
-            </b-col>
-        </b-row>
+                <b-col lg="4">
+                    <b-img class="bottom-space-needed" src="../assets/dev_work/2022 portfolio.png" fluid center></b-img>
+                </b-col>
+
+                <b-col lg="5" class="text-center center">
+
+                    <h3>2022 Portfolio</h3>
+                    <p>
+                        You are looking at this masterpiece at the moment! In the past I have used React for a lot of my projects, but I was really interested in other Frontend JavaScript frameworks. I decided to do this project in Vue to get another framework under my belt and understand the differences.
+                    </p>
+                    <p>
+                        This website is completely coded in Vue (JavaScript, HTML, CSS) with graphics I made myself. I am hoping to continue to add to this portfolio as I think of features or information that I would like to share.
+                    </p>
+                </b-col>
+                <b-col lg="3" class="text-center center">
+                    <b-button pill href="https://github.com/haylee430/site_haylee" class="button-format">Git Hub Repository</b-button>
+                </b-col>
+            </b-row>
+
+            <b-row>
+                <b-col lg="4">
+                    <b-img class="bottom-space-needed" src="../assets/dev_work/preoccupied_pup.png" fluid center></b-img>
+                </b-col>
+
+                <b-col lg="5" class="text-center center">
+
+                    <h3>Preoccupied Pup</h3>
+                    <p>
+                        A large passion of mine is animal welfare, so I wanted to create a health application for pups. I created a platform dedicated to tracking and managing your dog’s training schedule, eating schedule, medications, mood, grooming, and activities. This will help pet owners enhance the health and well-being of their dog(s).
+                    </p>
+                    <p>
+                        This application is mostly coded in Python but has some portions constructed in JavaScript, HTML, and CSS. This was a great learning experience as I got to investigate password hashing and other libraries that Python uses. </p>
+                </b-col>
+                <b-col lg="3" class="text-center center">
+                    <b-row>
+                        <b-col lg="12" sm="12">
+                            <b-button pill href="https://github.com/haylee430/preoccupied_pup" class="button-format">Git Hub Repository</b-button>
+                        </b-col>
+                        <b-col lg="12" sm="12">
+                            <b-button pill href="https://www.youtube.com/watch?v=VZGfBGbmEWY&t=25s" class="button-format top-space-needed">Video Presentation</b-button>
+                        </b-col>
+                    </b-row>
+                </b-col>
+            </b-row>
+
+            <b-row>
+                <b-col lg="4">
+                    <b-img class="bottom-space-needed" src="../assets/dev_work/2020 portfolio.png" fluid center></b-img>
+                </b-col>
+
+                <b-col lg="5" class="text-center center">
+
+                    <h3>2020 Portfolio</h3>
+                    <p>
+                        After my first bootcamp I was super excited to get into my first development role, but I did not have a portfolio yet. Before applying I raced to get something out there but was not sure what was the best approach for a fast portfolio project until someone suggested using a template. </p>
+                    <p>
+                        I found this amazing template online that I could use to create a temporary portfolio for myself, but I did not realize how hard it can be to deconstruct and change another person’s work. I struggled through this project but learned so much along the way such as clean coding practices, animation, and how to work with code I have never seen before. Looking back, it would have been better if I created my own, but it was a great learning experience and prepared be for working with legacy code. </p>
+                </b-col>
+                <b-col lg="3" class="text-center center">
+                    <b-row>
+                        <b-col lg="12" sm="12">
+                            <b-button pill href="https://github.com/haylee430/2020_portfolio" class="button-format">Git Hub Repository</b-button>
+                        </b-col>
+                        <b-col lg="12" sm="12">
+                            <b-button pill href="https://haylee430.github.io/2020_portfolio/" class="button-format top-space-needed">Visit the Site</b-button>
+                        </b-col>
+                    </b-row>
+                </b-col>
+            </b-row>
+
+            <b-row>
+                <b-col lg="4">
+                    <b-img class="bottom-space-needed" src="../assets/dev_work/fuzzy_friend_finder.png" fluid center></b-img>
+                </b-col>
+
+                <b-col lg="5" class="text-center center">
+
+                    <h3>Fuzzy Friend Finder</h3>
+                    <p>
+                        After my bootcamp experiences I realized I was missing the group coding projects so I reached out to some fellow bootcamp members to get a project started. We thought it would be a cool idea to redesign a rescues webpage in hopes of learning and potentially reaching out to help them with their site. </p>
+                    <p>
+                        This was a great experience to get some more collaboration in before becoming a true developer for a company and a lot was learned while working with graphics, React, and Github. Unfortunately, after reaching out to the rescue we never heard back! </p>
+                </b-col>
+                <b-col lg="3" class="text-center center">
+                    <b-row>
+                        <b-col lg="12" sm="12">
+                            <b-button pill href="https://github.com/haylee430/fuzzy_friend_finder" class="button-format">Git Hub Repository</b-button>
+                        </b-col>
+                        <b-col lg="12" sm="12">
+                            <b-button pill href="https://fuzzyfriendfinder.herokuapp.com/" class="button-format top-space-needed">Visit the Site</b-button>
+                        </b-col>
+                    </b-row>
+                </b-col>
+            </b-row>
+
+            <b-row>
+                <b-col lg="4">
+                    <b-img class="bottom-space-needed" src="../assets/dev_work/module_planner.png" fluid center></b-img>
+                </b-col>
+
+                <b-col lg="5" class="text-center center">
+
+                    <h3>Module Planner</h3>
+                    <p>
+                        Module Planner is a friendly planner application for users who are looking to become more organized. This project was done with some fellow boot campers. The large concept we dug into was how to use and code with REST API’s and databases. </p>
+                    <p>
+                        I would say this was one of my most challenging project as backend concepts have not come to me easily, but the experience overall has provided me with great knowledge for my current role at Target. </p>
+                </b-col>
+                <b-col lg="3" class="text-center center">
+                    <b-row>
+                        <b-col lg="12" sm="12">
+                            <b-button pill href="https://github.com/haylee430/module_planner" class="button-format">Git Hub Repository</b-button>
+                        </b-col>
+                        <b-col lg="12" sm="12">
+                            <b-button pill href="https://moduleplanner2020.herokuapp.com/" class="button-format top-space-needed">Visit the Site</b-button>
+                        </b-col>
+                    </b-row>
+                </b-col>
+            </b-row>
+
+            <b-row>
+                <b-col lg="4">
+                    <b-img class="bottom-space-needed" src="../assets/dev_work/smart_health.png" fluid center></b-img>
+                </b-col>
+
+                <b-col lg="5" class="text-center center">
+
+                    <h3>Smart Health</h3>
+                    <p>
+                        Though working with API’s can be hard it is much harder to work with an API from another company with only their docs to guide you. That is exactly what my group for this project wanted to explore. We were all currently interested in our smart watches and wanted to tap into our real time data to create a cool health application. <p>
+                        </p>
+                        <p>
+                            Again, this was extremely rewarding as it taught be how to read and use documentation provided by another company and really dig into small bugs as I was working. The moment we saw our live health data hit the screen was
+                        </p>
+                </b-col>
+                <b-col lg="3" class="text-center center">
+                    <b-row>
+                        <b-col lg="12" sm="12">
+                            <b-button pill href="https://smartheath.herokuapp.com/" class="button-format">Git Hub Repository</b-button>
+                        </b-col>
+                        <b-col lg="12" sm="12">
+                            <b-button pill href="https://github.com/haylee430/Project_3" class="button-format top-space-needed">Visit the Site</b-button>
+                        </b-col>
+                    </b-row>
+                </b-col>
+            </b-row>
+        </div>
+
+        <contact-section></contact-section>
 
     </b-container>
 
@@ -24,15 +167,15 @@
 
 <script>
 import NavBar from "@/components/NavBar";
-import ProjectTiles from './resume-components/ProjectTiles.vue';
 import Footer from "@/components/Footer";
+import ContactSection from './universal-components/ContactSection.vue';
 
 export default {
     name: "TechVideoGallery",
     components: {
         NavBar,
-        ProjectTiles,
-        Footer
+        Footer,
+        ContactSection
     },
 };
 </script>
